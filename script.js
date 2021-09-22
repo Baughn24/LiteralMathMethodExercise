98edb4efe065b1497956eea542fe7b88bb0b3b97
Allie Baughn <allison.baughn@gmail.com>
2021-09-22 17:54:33 -0500

    Completed LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+// 1b
+console.log(warmHugs.toUpperCase());
+// 1c
+console.log(warmHugs.replace("like", "love"));
+warmHugs = "Hi, I'm Olaf and I love warm hugs.";
+console.log(warmHugs);
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+// 2b
+console.log(beenImpaled.slice(18, 37));
+
+// 3a
+let dotDotDot = "...";
+// 3b
+let skullBones = 'I don\'t have a skull ${dotDotDot} or bones';
+console.log();
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random();
+randomNumber *= 4;
+console.log(randomNumber);
+
+// BONUS 6
+console.log(` Let It Go!`.repeat(2));
+
+// BONUS 7a
+let reindeers = "Reindeers are better than people";
+// BONUS 7b
+console.log(reindeers.replace(" ", "_"));
+
+// BONUS 8
+const number1 = 2;
+const result1 = Math.sqrt(2);
+console.log(`The square root of ${number1} is ${result1}`);
+
+// BONUS 9
+// AHHHHH I don't know!
